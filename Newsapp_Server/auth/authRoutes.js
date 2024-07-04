edb21368972748e0a6935b9867203775362d6933
SMPNayanajith <pasindusamarakoon500@gmail.com>
2024-07-04 23:24:49 +0530

    commit of authentication

--- Newsapp_Server/auth/authRoutes.js
+++ Newsapp_Server/auth/authRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
-const Reader = mongoose.model('Reader');
 const bcrypt = require('bcrypt');
 const JWT = require('jsonwebtoken');
+const Reader = mongoose.model('Reader');
 const AuthUser = mongoose.model('AuthUser');
+const Reporter = mongoose.model('Reporter');
 
 require('dotenv').config();
 
@@ -37,12 +38,110 @@ router.post('/add-new-reader', async (req, res) => {
             reader: reader._id
         });
         await authUser.save();
-
         res.send({ message: 'Reader registered successfully' });
     } catch (error) {
         console.log('Database error', error);
         return res.status(422).send({ error: error.message });
     }
 });
- 
+
+// Add new reporter
+router.post('/add-new-reporter', async (req, res) => {
+    try {
+        console.log('Data sent by client side-', req.body);
+        const { firstName, lastName, password, email, city, country, contactNumber } = req.body;
+
+        // Check if the email already exists
+        const existsReporter = await Reporter.findOne({ email });
+
+        if (existsReporter) {
+            return res.status(422).send({ error: "Email already exists" });
+        }
+
+        // Encrypt password
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        // Create new reporter object
+        const reporter = new Reporter({
+            firstName,
+            lastName,
+            password: hashedPassword,
+            email,
+            city,
+            country,
+            contactNumber
+        });
+
+        await reporter.save();
+
+        const authUser = new AuthUser({
+            userID: reporter._id, // Use the Reporter's _id as the userID
+            roleType: "reporter",
+            reporter: reporter._id
+        });
+        await authUser.save();
+        res.send({ message: 'Reporter registered successfully' });
+
+    } catch (error) {
+        console.log("Database error", error);
+        return res.status(422).send({ error: error.message });
+    }
+});
+
+// Login router
+router.post('/user-login', async (req, res) => {
+    try {
+        const { userID, password } = req.body;
+        const authUser = await AuthUser.findOne({ userID });
+        if (!authUser) {
+            return res.status(401).json({ error: 'User not found' });
+        }
+        
+        let user;
+        if(authUser.roleType==="reader")
+            {
+                user=await Reader.findById(authUser.reader);
+            }else if(authUser.roleType==="reporter")
+                {
+                    user = await Reporter.findById(authUser.reporter);
+
+                }
+          //if no user found
+        if(!user)
+            {
+                return res.status(401).json({ error: 'User not found' });
+            }
+
+        //if found user
+        const isMatch = await bcrypt.compare(password,user.password);
+
+        if(!isMatch)
+            {
+                return res.status(401).json({ error: 'Invalid password' });
+            }
+        const token =JWT.sign({userID:authUser._id,roleType:authUser.roleType},process.env.JWT_SECRET,{expiresIn:'10m'});
+        const roleType = authUser.roleType;
+
+        //return data
+        res.json({token,roleType});
+        console.log(`Logging as ${authUser.roleType}`);
+
+        
+
+        // const isPasswordValid = await bcrypt.compare(password, authUser.password);
+        // if (!isPasswordValid) {
+        //     return res.status(401).send({ error: 'Invalid password' });
+        // }
+
+        // const token = JWT.sign({ userID: authUser.userID }, process.env.JWT_SECRET, {
+        //     expiresIn: '1h'
+        // });
+
+        res.send({ message: 'Login successful', token });
+    } catch (error) {
+        console.log('Database error', error);
+        return res.status(500).send({ error: error.message });
+    }
+});
+
 module.exports = router;
---
+++ Newsapp_Server/middleware/authMiddleware.js
@@ -0,0 +1,25 @@
+const JWT =require("jsonwebtoken");
+require ('dotenv').config();
+
+const authMiddleware =(req,res,next)=>{
+    const authHeader = req.header("Authorization");
+    const token = authHeader.split("")[1];
+    if(!token)
+        {
+            return res.status(401).json ({error:"Token not found"});
+        }
+
+        //if found
+    try{
+
+        const decode =JWT.verify(token,process.env.JWT_SECRET   );
+        req.userID = decode.userID;
+        next();
+
+    }catch(error){
+        console.log(error)
+    }
+
+}
+
+modele.exports = authMiddleware;
